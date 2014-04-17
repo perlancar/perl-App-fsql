f4c279ff75baa78b0bede90278aa78954c687052
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-04-17 15:12:12 +0700

    Initial commit, incomplete

---
+++ lib/App/fsql.pm
@@ -0,0 +1,13 @@
+package App::fsql;
+
+# VERSION
+# DATE
+
+1;
+# ABSTRACT: Perform SQL queries against files in CSV/TSV/LTSV/JSON/YAML formats
+
+=head1 SYNOPSIS
+
+See the command-line script L<fsql>.
+
+=cut
