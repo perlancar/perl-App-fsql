d757c0582a9a77fadb7f8f00f58ccceaf8589d92
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-09-10 12:16:19 +0700

    Mention App::tsql

--- lib/App/fsql.pm
+++ lib/App/fsql.pm
@@ -1,7 +1,11 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
 package App::fsql;
 
 # VERSION
+# AUTHORITY
 # DATE
+# DIST
 
 1;
 # ABSTRACT: Perform SQL queries against files in CSV/TSV/LTSV/JSON/YAML formats
@@ -9,5 +13,11 @@ package App::fsql;
 =head1 SYNOPSIS
 
 See the command-line script L<fsql>.
+
+
+=head1 SEE ALSO
+
+L<tsql> (from L<App::tsql>) a fork which uses SQLite as backend instead of
+L<DBD::CSV> and L<SQL::Statement>.
 
 =cut
